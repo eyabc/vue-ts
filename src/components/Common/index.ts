fcb89fbf367f5c0c564619aaa3402f7ea769cea2
eyabc <bey4314@naver.com>
2020-03-25 14:44:35 +0900

    class-based component

---
+++ src/components/Common/index.ts
@@ -0,0 +1,2 @@
+export { default as Message } from './Message.vue';
+export { default as HelloWorld } from './HelloWorld.vue';
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hello-world msg="Welcome to Your Vue.js App" />
+    <message />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { HelloWorld, Message } from '@/components';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
-}
+    Message, HelloWorld,
+  },
+};
 </script>
