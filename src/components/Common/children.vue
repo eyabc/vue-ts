f944c423f8e12d2a0dfe01846bade419eccc9122
eyabc <bey4314@naver.com>
2020-03-25 17:15:22 +0900

    @Provide, @inject

--- src/components/Common/children.vue
+++ src/components/Common/children.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-        <button @click="counter">자식에서 숫자를 증가시킵니다.</button>
+        {{ message }}
     </div>
 </template>
 
 <script lang="ts">
-  import {Component, Prop, Vue, Watch, Emit} from 'vue-property-decorator';
+  import {Component, Vue, Inject} from 'vue-property-decorator';
 
   @Component
   export default class Children extends Vue {
-    @Emit()
-    counter() { console.log('counter emit') }
+    // 초기화를 안하면 lint 에서 에러를 나게 한다.
+    // ! 는 값이 초기화를 안한것이 의도적인것으로 인식함.
+    // readonly 부모에서 받아오는 값으로 변경이 되어서는 안된다.
+    @Inject() readonly message!: string;
   }
 
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="home">
-    <children @counter="counter" />
-    <p>
-      부모에서 숫자를 보여줍니다 : {{ count }}
-    </p>
+    <children></children>
   </div>
 </template>
 
 <script lang="ts">
 import { Children } from '@/components/Common';
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Provide } from 'vue-property-decorator';
 @Component({
   components: {
     Children
   },
 })
 export default class Home extends Vue {
-  count:number = 0;
-
-  counter() {
-    this.count++;
-  }
+  // 부모에서 주는 변수이름과 자식의 변수이름이 다를 때 괄호의 String으로 매핑시킬 수 있다.
+  @Provide('message') msg: string = 'provide/inject example';
 }
+// prop를 쓸 땐 직계 부모에서만 쓸 수 있었다. 부모의 부모의 부모... 에게 주입받을 수 있다.
+// 컴포넌트에 직접 넘겨주는 방식과 달리 어떤 컴포넌트로 직관적으로 넘어가는지 파악하기 어렵다.
+// 일반로직에서는 권장되지 않는다.
 </script>
+
