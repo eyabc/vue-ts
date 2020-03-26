119110abfa53c636c900ea763457ccd4a9bfbc16
eyabc <bey4314@naver.com>
2020-03-26 16:39:06 +0900

    vuex interface store

--- vuex/src/App.vue
+++ vuex/src/App.vue
@@ -1,29 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="increase">증가</button>
+    <button @click="decrease">감소</button>
+    <count></count>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
+import count from '@/components/count.vue'
 @Component({
   components: {
-    HelloWorld,
+    count
   },
 })
-export default class App extends Vue {}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+export default class App extends Vue {
+  increase() {
+    this.$store.dispatch('increase');
+  }
+  decrease() {
+    this.$store.dispatch('decrease');
+  }
 }
-</style>
+</script>
---
+++ vuex/src/components/count.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{ $store.getters.count }}
+    </div>
+</template>
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Count extends Vue { }
+</script>
