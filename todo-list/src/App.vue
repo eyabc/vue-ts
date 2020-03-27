06a203df47eef4ec7aa1b76b797e671fdc74cfda
eyabc <bey4314@naver.com>
2020-03-27 11:42:34 +0900

    components

---
+++ todo-list/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <h1>TODO LIST</h1>
+        <nav-header></nav-header>
+        <item-input></item-input>
+        <item></item>
+    </div>
+</template>
+
+<script lang="ts">
+  import {Vue, Component} from 'vue-property-decorator';
+  import * as common from '@/components';
+  // import { NavHeader, Item, ItemInput } from '@/components';
+
+  @Component({
+    components: { ...common }
+  })
+  export default class App extends Vue {
+
+  }
+</script>
+
+<style>
+    #app {
+        padding: 100px;
+    }
+</style>
---
+++ todo-list/src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as NavHeader } from './header.vue';
+export { default as Item } from './item.vue';
+export { default as ItemInput } from './item-input.vue';
+
